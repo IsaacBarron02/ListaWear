8bc2157efe61af79ea180496f968ab672c3e8f9a
IsaacBarron02 <59702770+IsaacBarron02@users.noreply.github.com>
2021-07-11 21:06:45 -0500

    Actualizacion se cambio el Toast por una notificacion

--- app/src/main/java/com/example/listawear/CardActivity.java
+++ app/src/main/java/com/example/listawear/CardActivity.java
@@ -1,17 +1,31 @@
 package com.example.listawear;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import static android.provider.CalendarContract.EXTRA_EVENT_ID;
+
 public class CardActivity extends Activity {
     private TextView titulo;
     private TextView descripcion;
     private Button cerrar;
-    private Button enrolar;
+    private Button btNotificacion;
+    private final static String CHANNEL_ID="NOTIFICACION";
+    private final static int NOTIFICATION_ID=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -21,7 +35,7 @@ public class CardActivity extends Activity {
         titulo = (TextView) findViewById(R.id.txtTitulo);
         descripcion = (TextView) findViewById(R.id.txtDescripcion);
         cerrar = (Button) findViewById(R.id.btnCerrar);
-        enrolar = (Button) findViewById(R.id.btnEnrolar);
+        btNotificacion = (Button) findViewById(R.id.btnEnrolar);
 
         Bundle extras = getIntent().getExtras();
         if( extras != null){
@@ -35,14 +49,33 @@ public class CardActivity extends Activity {
                 onBackPressed();
             }
         });
-        enrolar.setOnClickListener(new View.OnClickListener() {
+        btNotificacion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(CardActivity.this,
-                        "Te enrolaste",
-                        Toast.LENGTH_LONG).show();
+                createNotificationChannel();
+                createNotification();
             }
         });
+    }
+    private void createNotificationChannel() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            CharSequence name ="Notificacion";
+            NotificationChannel notificationChannel=new NotificationChannel(CHANNEL_ID, name, NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager notificationManager=(NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+    }
+    private void createNotification(){
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), CHANNEL_ID);
+        builder.setSmallIcon(R.drawable.ic_baseline_notifications_active_24);
+        builder.setContentTitle("Notificacion de inscripción");
+        builder.setContentText("Felicidades, te has registrado");
+        builder.setColor(Color.BLACK);
+        builder.setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        builder.setDefaults(Notification.DEFAULT_SOUND);
+
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(getApplicationContext());
+        notificationManagerCompat.notify(NOTIFICATION_ID, builder.build());
     }
 
 }
